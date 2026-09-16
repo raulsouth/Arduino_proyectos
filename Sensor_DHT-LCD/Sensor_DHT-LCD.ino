a36b737f6765e3fd8ee54b7cb92b93eb4e53f314
raulsouth <raulsouth@gmail.com>
2026-09-15 23:15:56 -0300

    Agrego un programa para el sensor DHT11

---
+++ Sensor_DHT-LCD/Sensor_DHT-LCD.ino
@@ -0,0 +1,66 @@
+//Programa para medir la temperatura y la humedad de un ambiente
+//que se muestra en una pantalla lcd 16x2 I2C.
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int SENSOR = 8;
+float TEMPERATURA;
+float HUMEDAD;
+
+
+DHT dht (SENSOR, DHT11);
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Iniciando jeje");
+  dht.begin();
+
+  delay(3000);
+  lcd.clear();
+
+}
+
+void loop() {
+  TEMPERATURA = dht.readTemperature();
+  HUMEDAD = dht.readHumidity();
+
+  Serial.println(TEMPERATURA);
+  Serial.println(HUMEDAD);
+  delay(1000);
+
+  if (TEMPERATURA <= 21 & HUMEDAD <= 61){
+    lcd.setCursor(0,0);
+    lcd.print("Temp: ");
+    lcd.setCursor(9, 0);
+    lcd.print(TEMPERATURA);
+
+    lcd.setCursor(0,1);
+    lcd.print("Humedad: ");
+    lcd.setCursor(9, 1);
+    lcd.print(HUMEDAD);
+
+
+    delay(1000);
+    lcd.clear();
+
+  }
+  
+  
+  if (TEMPERATURA > 21 & HUMEDAD > 61){
+    lcd.setCursor(0,0);
+    lcd.print("T: mas de 20");
+    lcd.setCursor(0,1);
+    lcd.print("H: mas de 60");
+    //lcd.clear();
+
+  }
+
+}
